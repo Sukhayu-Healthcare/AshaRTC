7d93369d30a839e209ff8d48e7baff77ccf0a1ed
Shlok-Dubey <dubeyshlok12@gmail.com>
2025-12-02 00:24:10 +0530

    done2

--- src/redis.ts
+++ src/redis.ts
@@ -2,10 +2,6 @@ import { createClient } from 'redis';
 
 export const redis = createClient({
   url: "redis://default:Z46eGq3UaNwUpZ7Nez5iOQwzm2A3zsQb@redis-17425.c256.us-east-1-2.ec2.cloud.redislabs.com:17425",
-  socket: {
-    tls: true,
-    rejectUnauthorized: false
-  }
 });
 
 redis.on('error', err => console.log('Redis Client Error', err));
